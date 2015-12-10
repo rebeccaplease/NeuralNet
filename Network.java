fbf3e717ca63bdfbe0ad4948ce4d13a6eec7d199
Rebecca <rebeccaplease@gmail.com>
2015-12-10 13:14:10 -0500

    comments

--- Network.java
+++ Network.java
@@ -1,5 +1,7 @@
+/**
+ * For holding nodes in each layer (input, hidden, and output)
+ */
 public class Network {
-   //arraylist
    Node[] inputLayer;
    Node[] hiddenLayer;
    Node[] outputLayer;
@@ -10,24 +12,3 @@ public class Network {
       outputLayer = new Node[no];
    }
 }
-// private static class Network {
-//   //arraylist
-//   ArrayList<Node[]> layers = new ArrayList<Node[]>();
-//   int numLayers = 3;
-//   public Network(int ni, HiddenLayers h, int no){
-//     layers.add(new Node[ni]); //inputLayer
-//     //hidden layer(s)
-//     for(k = 0; k < h.nodes.length; k++){
-//       hiddenLayer = new Node[nh];
-//       layers.add(new Node[h.])
-//     }
-//
-//     layers.add(new Node[no]); //outputLayer
-//   }
-// }
-// private static class HiddenLayers{
-//   int[] nodes;
-//   public HiddenLayers(int[] )
-//
-// }
-//for each
